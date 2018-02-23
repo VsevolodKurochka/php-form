a10e34ec1cc44037848c8260a1d47ce9bc7999b1
Vsevolod Kurochka <dramdoobas1@gmail.com>
2018-02-23 13:57:57 +0200

    Create more beautifl html-letter

--- mail.php
+++ mail.php
@@ -2,7 +2,6 @@
 	header("Access-Control-Allow-Origin: *");
 	if($_SERVER["REQUEST_METHOD"] == "POST"){
 
-		//print_r($_POST);
 		$default = array(
 			'email' 		=> 'seva.kurochka@gmail.com',
 			'sendfrom' 	=> 'ourLanding@gmail.com',
@@ -36,29 +35,36 @@
 			return $headers;
 		}
 
-		function createMessage($message_fields){
+		function createMessage($message_fields, $settings){
 			$message 	= '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">';
 			$message .= '<html xmlns="http://www.w3.org/1999/xhtml">';
 			$message .= '<head><meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/></head>';
 			$message .= '<body>';
 			$message .= '<div style="background-color:#f7f7f7;margin:0;padding:70px 0 70px 0;width:100%;">';
 				$message .= '<div style="width: 600px; max-width: 100%; margin-left: auto; margin-right: auto;">';
-					$message .= '<h1 style="background-color: #96588a; color: #fff; font-weight: bold; 	font-family: "Helvetica Neue",Helvetica,Roboto,Arial,sans-serif;">'.$_POST['info']['title'].'</h1>';
-					$message .= '<table rules="all" style="border: 1px solid #999; width: 100%" cellpadding="10">';
+					$message .= '<h1 style="background-color: #96588a; color: #fff; font-weight: bold; padding: 20px 30px; margin-bottom: 0;">'.$_POST['info']['title'].'</h1>';
+					$message .= '<div style="padding: 35px; background-color: #fff;">';
+						$message .= '<p style="margin-top: 0; letter-spacing: 1px;">'.$_POST['info']['subtitle'].'</p>';
 
-						foreach($message_fields as $field => $field_key) {
+						$message .= '<table rules="all" style="border: 1px solid #e5e5e5; width: 100%; color: #636363; margin-bottom: 60px;" cellpadding="15">';
 
-							$contact_field = $_POST['contacts'][$field];
+							foreach($message_fields as $field => $field_key) {
 
-							if( !empty($contact_field) && isset($contact_field) ){
-								$message .= '<tr>';
-								$message .= '<td><b>'.$field_key['title'].'</b></td>';
-								$message .= '<td>'.strip_tags($contact_field).'</td>';
-								$message .= '</tr>';
+								$contact_field = $_POST['contacts'][$field];
+
+								if( !empty($contact_field) && isset($contact_field) ){
+									$message .= '<tr>';
+									$message .= '<th style="text-align: left;">'.$field_key['title'].'</th>';
+									$message .= '<td>'.strip_tags($contact_field).'</td>';
+									$message .= '</tr>';
+								}
 							}
-						}
 
-					$message .= '</table>';
+						$message .= '</table>';
+
+						$message .= '<p style="text-align: center; font-size: 12px; color: #c09bb9;">'.$settings['subject'].'</p>';
+
+					$message .= '</div>';
 				$message .= '</div>';
 			$message .= '</div>';
 			$message .= '</body></html>';
@@ -71,8 +77,7 @@
 		$sendfrom 	= $default['sendfrom'];
 		
 		$headers = createHeaders($sendfrom);
-		$message = createMessage($fields);
-
+		$message = createMessage($fields, $settings);
 
 
 		$send  			= mail($to, $subject, $message, $headers);
@@ -80,5 +85,4 @@
 	}else{
 		die('No direct access');
 	}
-	//${"data_{$field}"}
 ?>
