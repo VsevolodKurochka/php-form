fb5e8aead462e1641f1558e9076e6026e38a7958
Vsevolod Kurochka <dramdoobas1@gmail.com>
2018-02-24 13:38:20 +0200

    Start Working with OOP mail

--- mail.php
+++ mail.php
@@ -1,88 +1,94 @@
 <?php
 	header("Access-Control-Allow-Origin: *");
-	if($_SERVER["REQUEST_METHOD"] == "POST"){
+	require_once 'mail/mail.php';
 
-		$default = array(
-			'email' 		=> 'seva.kurochka@gmail.com',
-			'sendfrom' 	=> 'ourLanding@gmail.com',
-			'subject'  	=> 'Site Subject'
-		);
+	$form_mail = new SendMail('to', 'from', 'subject');
 
-		$settings = array(
-			'email' 		=> ( !empty($_POST['change_email']) && isset($_POST['change_email']) ) ? $_POST['change_email'] : $default['email'],
-			'subject' 	=> ( !empty($_POST['info']['subject']) && isset($_POST['info']['subject']) ) ? $_POST['info']['subject'] : $default['subject']
-		);
+	//print_r($form_mail);
+	//echo $form_mail->get_to();
+	// if($_SERVER["REQUEST_METHOD"] == "POST"){
 
-		$fields = array(
-			'name' 			=> array(
-				'title' 		=> 'Name'
-			),
-			'email' 			=> array(
-				'title' 		=> 'Email'
-			), 
-			'phone' 			=> array(
-				'title'			=> 'Phone'
-			)
-		);
+	// 	$default = array(
+	// 		'email' 		=> 'seva.kurochka@gmail.com',
+	// 		'sendfrom' 	=> 'ourLanding@gmail.com',
+	// 		'subject'  	=> 'Site Subject'
+	// 	);
 
-		function createHeaders($from){
-			$headers  = "From: " . strip_tags($from) . "\r\n";
-			$headers .= "Reply-To: ". strip_tags($from) . "\r\n";
-			$headers .= "MIME-Version: 1.0\r\n";
-			$headers .= "Content-Type: text/html;charset=utf-8 \r\n";
-			$headers .= "Content-Transfer-Encoding: 8bit \r\n";
+	// 	$settings = array(
+	// 		'email' 		=> ( !empty($_POST['change_email']) && isset($_POST['change_email']) ) ? $_POST['change_email'] : $default['email'],
+	// 		'subject' 	=> ( !empty($_POST['info']['subject']) && isset($_POST['info']['subject']) ) ? $_POST['info']['subject'] : $default['subject']
+	// 	);
 
-			return $headers;
-		}
+	// 	$fields = array(
+	// 		'name' 			=> array(
+	// 			'title' 		=> 'Name'
+	// 		),
+	// 		'email' 			=> array(
+	// 			'title' 		=> 'Email'
+	// 		), 
+	// 		'phone' 			=> array(
+	// 			'title'			=> 'Phone'
+	// 		)
+	// 	);
 
-		function createMessage($message_fields, $settings){
-			$message 	= '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">';
-			$message .= '<html xmlns="http://www.w3.org/1999/xhtml">';
-			$message .= '<head><meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/></head>';
-			$message .= '<body>';
-			$message .= '<div style="background-color:#f7f7f7;margin:0;padding:70px 0 70px 0;width:100%;">';
-				$message .= '<div style="width: 600px; max-width: 100%; margin-left: auto; margin-right: auto;">';
-					$message .= '<h1 style="background-color: #96588a; color: #fff; font-weight: bold; padding: 20px 30px; margin-bottom: 0;">'.$_POST['info']['title'].'</h1>';
-					$message .= '<div style="padding: 35px; background-color: #fff;">';
-						$message .= '<p style="margin-top: 0; letter-spacing: 1px;">'.$_POST['info']['subtitle'].'</p>';
+	// 	function createHeaders($from){
+	// 		$headers  = "From: " . strip_tags($from) . "\r\n";
+	// 		$headers .= "Reply-To: ". strip_tags($from) . "\r\n";
+	// 		$headers .= "MIME-Version: 1.0\r\n";
+	// 		$headers .= "Content-Type: text/html;charset=utf-8 \r\n";
+	// 		$headers .= "Content-Transfer-Encoding: 8bit \r\n";
 
-						$message .= '<table rules="all" style="border: 1px solid #e5e5e5; width: 100%; color: #636363; margin-bottom: 60px;" cellpadding="15">';
+	// 		return $headers;
+	// 	}
 
-							foreach($message_fields as $field => $field_key) {
+	// 	function createMessage($message_fields, $settings){
+	// 		$message 	= '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">';
+	// 		$message .= '<html xmlns="http://www.w3.org/1999/xhtml">';
+	// 		$message .= '<head><meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/></head>';
+	// 		$message .= '<body>';
+	// 		$message .= '<div style="background-color:#f7f7f7;margin:0;padding:70px 0 70px 0;width:100%;">';
+	// 			$message .= '<div style="width: 600px; max-width: 100%; margin-left: auto; margin-right: auto;">';
+	// 				$message .= '<h1 style="background-color: #96588a; color: #fff; font-weight: bold; padding: 20px 30px; margin-bottom: 0;">'.$_POST['info']['title'].'</h1>';
+	// 				$message .= '<div style="padding: 35px; background-color: #fff;">';
+	// 					$message .= '<p style="margin-top: 0; letter-spacing: 1px;">'.$_POST['info']['subtitle'].'</p>';
 
-								$contact_field = $_POST['contacts'][$field];
+	// 					$message .= '<table rules="all" style="border: 1px solid #e5e5e5; width: 100%; color: #636363; margin-bottom: 60px;" cellpadding="15">';
 
-								if( !empty($contact_field) && isset($contact_field) ){
-									$message .= '<tr>';
-									$message .= '<th style="text-align: left;">'.$field_key['title'].'</th>';
-									$message .= '<td>'.strip_tags($contact_field).'</td>';
-									$message .= '</tr>';
-								}
-							}
+	// 						foreach($message_fields as $field => $field_key) {
 
-						$message .= '</table>';
+	// 							$contact_field = $_POST['contacts'][$field];
 
-						$message .= '<p style="text-align: center; font-size: 12px; color: #c09bb9;">'.$settings['subject'].'</p>';
+	// 							if( !empty($contact_field) && isset($contact_field) ){
+	// 								$message .= '<tr>';
+	// 								$message .= '<th style="text-align: left;">'.$field_key['title'].'</th>';
+	// 								$message .= '<td>'.strip_tags($contact_field).'</td>';
+	// 								$message .= '</tr>';
+	// 							}
+	// 						}
 
-					$message .= '</div>';
-				$message .= '</div>';
-			$message .= '</div>';
-			$message .= '</body></html>';
+	// 					$message .= '</table>';
 
-			return $message;	
-		}
+	// 					$message .= '<p style="text-align: center; font-size: 12px; color: #c09bb9;">'.$settings['subject'].'</p>';
 
-		$to 				= $settings['email']; 
-		$subject 		= $settings['subject'];
-		$sendfrom 	= $default['sendfrom'];
+	// 				$message .= '</div>';
+	// 			$message .= '</div>';
+	// 		$message .= '</div>';
+	// 		$message .= '</body></html>';
+
+	// 		return $message;	
+	// 	}
+
+	// 	$to 				= $settings['email']; 
+	// 	$subject 		= $settings['subject'];
+	// 	$sendfrom 	= $default['sendfrom'];
 		
-		$headers = createHeaders($sendfrom);
-		$message = createMessage($fields, $settings);
+	// 	$headers = createHeaders($sendfrom);
+	// 	$message = createMessage($fields, $settings);
 
 
-		$send  			= mail($to, $subject, $message, $headers);
+	// 	$send  			= mail($to, $subject, $message, $headers);
 		
-	}else{
-		die('No direct access');
-	}
+	// }else{
+	// 	die('No direct access');
+	// }
 ?>
---
+++ mail/mail.php
@@ -0,0 +1,69 @@
+<?php
+	/**
+	* SendMail
+	*/
+	class SendMail
+	{
+		//public $to;
+		//public $from;
+		//public $subject;
+
+
+		function __construct($to, $from, $subject){
+			$this->to = $to;
+			$this->from = $from;
+			$this->subject = $subject;
+		}
+
+		private function build_headers() {
+			$headers  = "From: " . strip_tags($this->from) . "\r\n";
+			$headers .= "Reply-To: ". strip_tags($this->from) . "\r\n";
+			$headers .= "MIME-Version: 1.0\r\n";
+			$headers .= "Content-Type: text/html;charset=utf-8 \r\n";
+			$headers .= "Content-Transfer-Encoding: 8bit \r\n";
+
+			return $headers;
+		}
+
+		private function build_body() {
+			$message 	= '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">';
+			$message .= '<html xmlns="http://www.w3.org/1999/xhtml">';
+			$message .= '<head><meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/></head>';
+			$message .= '<body>';
+			$message .= '<div style="background-color:#f7f7f7;margin:0;padding:70px 0 70px 0;width:100%;">';
+				$message .= '<div style="width: 600px; max-width: 100%; margin-left: auto; margin-right: auto;">';
+					$message .= '<h1 style="background-color: #96588a; color: #fff; font-weight: bold; padding: 20px 30px; margin-bottom: 0;">'.$_POST['info']['title'].'</h1>';
+					$message .= '<div style="padding: 35px; background-color: #fff;">';
+						$message .= '<p style="margin-top: 0; letter-spacing: 1px;">'.$_POST['info']['subtitle'].'</p>';
+
+						$message .= '<table rules="all" style="border: 1px solid #e5e5e5; width: 100%; color: #636363; margin-bottom: 60px;" cellpadding="15">';
+
+							foreach($message_fields as $field => $field_key) {
+
+								$contact_field = $_POST['contacts'][$field];
+
+								if( !empty($contact_field) && isset($contact_field) ){
+									$message .= '<tr>';
+									$message .= '<th style="text-align: left;">'.$field_key['title'].'</th>';
+									$message .= '<td>'.strip_tags($contact_field).'</td>';
+									$message .= '</tr>';
+								}
+							}
+
+						$message .= '</table>';
+
+						$message .= '<p style="text-align: center; font-size: 12px; color: #c09bb9;">'.$settings['subject'].'</p>';
+
+					$message .= '</div>';
+				$message .= '</div>';
+			$message .= '</div>';
+			$message .= '</body></html>';
+
+			return $message;
+		}
+
+		public function send() {
+			return wp_mail($this->to, $this->subject, $)
+		}
+	}
+?>
